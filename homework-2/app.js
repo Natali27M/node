a391b9fce23061e87667480856a8b042baa6cde6
Natali27M <vns122716@gmail.com>
2022-02-18 14:33:14 +0200

    класна 2

--- homework-2/app.js
+++ homework-2/app.js
@@ -1,85 +1,3 @@
-// декілька ендпоінтів зробити
-//
-// 1. /login, поля які треба відрендерити в файлі hbs: firstName, lastName, email(унікальне поле), password, age, city
-// просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися в масив і редірект робити на сторінку
-// з усіма юзерами /users і перевірка чи такий імейл не існує, якщо існує то редірект на еррор пейдж
-//
-// 2. /users просто сторінка з усіма юзерами, але можна по квері параметрам їх фільтрувати по age і city
-//
-// 3. /user/:id сторінка з інфою про одного юзера
-//
-// 4. зробити якщо не відпрацюють ендпоінти то на сторінку notFound редірект
-
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const {engine} = require('express-handlebars');
-//
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.static(path.join(__dirname, 'static')));
-//
-// app.set('view engine', '.hbs');
-// app.engine('.hbs', engine({ defaultLayout: false }));
-// app.set('views', path.join(__dirname, 'static'));
-//
-// let users = [{id: 1,firstName:'Natalya',lastName:'Malitska', email: 'vns122716@gmail.com', password: '12345',age:37,city:'Lviv'}]
-// let error = '';
-//
-// app.get('/login', (req, res) => {
-//     res.render('login');
-// });
-//
-// app.post('/login', ({ body }, res) => {
-//     const userExist = users.some(user => user.email === body.email);
-//     if (userExist) {
-//         error = 'User with this email exist!';
-//         res.redirect('/error');
-//         return;
-//     }
-//
-//     users.push({ ...body, id: users.length ? users[users.length - 1].id + 1 : 1 });
-//     res.redirect('/users');
-// });
-//
-// app.get('/users', ({ query }, res) => {
-//     if (Object.keys(query).length) {
-//         let usersArray = [...users];
-//         if (query.city) {
-//             usersArray = usersArray.filter(user => user.city === query.city);
-//         }
-//         if (query.age) {
-//             usersArray = usersArray.filter(user => user.age === query.age);
-//         }
-//
-//         res.render('users', { users: usersArray });
-//         return;
-//     }
-//
-//     res.render('users', { users });
-// });
-//
-// app.get('/users/:userId', (req, res) => {
-//     const {userId} = req.params;
-//
-//     const user = users.find(user => user.id === +userId);
-//     if (!user) {
-//         error = `User with ID: ${userId} exist!`;
-//         res.render('error', {error});
-//         return;
-//     }
-//
-//     res.render('userDetails', { user });
-// });
-//
-// app.use((req,res)=>{
-//     res.render('notFound');
-// })
-//
-// app.listen(5200, () => {
-//     console.log('Serves has started on PORT: http://localhost:5200');
-// });
-
 // Необхідно розширити ваше ДЗ:
 //     - додайте ендпоінт signIn який буде приймати email і password і якщо все вірно то редірект на сторінку цього
 //
@@ -96,17 +14,44 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.set('view engine', '.hbs');
-app.engine('.hbs', engine({ defaultLayout: false }));
+app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
-let users = [{id: 1,firstName:'Natalya',lastName:'Malitska', email: 'vns122716@gmail.com', password: '12345',age:37,city:'Lviv'}];
+let users = [{
+    id: 1,
+    firstName: 'Natalya',
+    lastName: 'Malitska',
+    email: 'vns122716@gmail.com',
+    password: '12345',
+    age: 37,
+    city: 'Lviv'
+    },
+    {
+        id: 2,
+        firstName: 'Oleg',
+        lastName: 'Petrov',
+        email: 'Oleg@gmail.com',
+        password: '11111',
+        age: 22,
+        city: 'Kyiv'
+    },
+    {
+        id: 3,
+        firstName: 'Ira',
+        lastName: 'Litna',
+        email: 'ira@gmail.com',
+        password: '22222',
+        age: 35,
+        city: 'Odessa'
+    }];
+
 let error = '';
 
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.post('/login', ({ body }, res) => {
+app.post('/login', ({body}, res) => {
     const userExist = users.some(user => user.email === body.email);
     if (userExist) {
         error = 'User with this email exist!';
@@ -114,11 +59,11 @@ app.post('/login', ({ body }, res) => {
         return;
     }
 
-    users.push({ ...body, id: users.length ? users[users.length - 1].id + 1 : 1 });
+    users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1});
     res.redirect('/users');
 });
 
-app.get('/users', ({ query }, res) => {
+app.get('/users', ({query}, res) => {
     if (Object.keys(query).length) {
         let usersArray = [...users];
         if (query.city) {
@@ -128,11 +73,11 @@ app.get('/users', ({ query }, res) => {
             usersArray = usersArray.filter(user => user.age === query.age);
         }
 
-        res.render('users', { users: usersArray });
+        res.render('users', {users: usersArray});
         return;
     }
 
-    res.render('users', { users });
+    res.render('users', {users});
 });
 
 app.get('/users/:userId', (req, res) => {
@@ -144,22 +89,21 @@ app.get('/users/:userId', (req, res) => {
         res.render('error', {error});
         return;
     }
-    res.render('userDetails', { user });
+    res.render('userDetails', {user});
 });
 
 app.post('/users/:userId', ({params}, res) => {
-    users = users.filter(user => user.id !== +params.id);
+    users = users.filter(user => user.id !== +params.userId);
     res.redirect('/users');
 });
 
-//     - додайте ендпоінт signIn який буде приймати email і password і якщо все вірно то редірект на сторінку цього
 app.get('/singIn', (req, res) => {
     res.render('singIn');
 });
 
 app.post('/singIn', ({body}, res) => {
     const userEmailPassword = users.find(user => user.email === body.email && user.password === body.password);
-    if(userEmailPassword){
+    if (userEmailPassword) {
         res.redirect(`/users/${userEmailPassword.id}`);
     }
 });
@@ -171,5 +115,3 @@ app.use((req, res) => {
 app.listen(5200, () => {
     console.log('Serves has started on PORT: http://localhost:5200');
 });
-// * хто хоче складніше реалізуйте видалення користувача. Кнопка повинна знаходитись на сторінці з інфою про одного
-// юзера. Після видалення редірект на "/users"
