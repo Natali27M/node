34ffb9e7b393745c077bc35283486a04f0318c33
Natali27M <vns122716@gmail.com>
2022-02-22 19:20:24 +0200

    домашня 3

---
+++ homework-4/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import {Request,Response} from 'express';
+
+const app = express();
+
+app.get('/', (req:Request,res:Response)=>{
+    res.end();
+});
+
+app.listen(5500,()=>{
+    console.log('Serves has started on PORT: http://localhost:5500');
+});
