5cab55f164e3fe433e00b41b6539d5644bdd18f8
Natali27M <vns122716@gmail.com>
2022-05-17 15:10:06 +0300

    домашня 14

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { userService } from "./services";
+
+const App = () => {
+    const { reset, handleSubmit, register } = useForm();
+    const [user, setUser] = useState(null);
+    const emailInput = useRef();
+
+    const submit = async (user) => {
+        await userService.create(user);
+        reset();
+    }
+
+    const getByEmail = async () => {
+        const email = emailInput.current.value;
+        const {data} = await userService.getByEmail(email);
+        setUser(data)
+    }
+
+  return (
+      <div>
+          <form onSubmit={ handleSubmit(submit) }>
+              <div><label>firstName: <input type="text" { ...register('firstName') }/></label></div>
+              <div><label>lastName: <input type="text" { ...register('lastName') }/></label></div>
+              <div><label>age: <input type="text" { ...register('age') }/></label></div>
+              <div><label>phone: <input type="text" { ...register('phone') }/></label></div>
+              <div><label>email: <input type="text" { ...register('email') }/></label></div>
+              <div><label>password: <input type="text" { ...register('password') }/></label></div>
+              <button>Create</button>
+          </form>
+          <div>
+              <input type="text" placeholder={'email'} ref={ emailInput }/>
+              <button onClick={ getByEmail }>getByEmail</button>
+          </div>
+          <hr/>
+          <div>
+              { user && JSON.stringify(user) }
+          </div>
+      </div>
+  );
+};
+
+export { App };
---
+++ client/src/services/user.service.js
@@ -0,0 +1,8 @@
+import { axiosService } from "./axios.service";
+
+import { urls } from "../constants";
+
+export const userService = {
+    create: (user) => axiosService.post(urls.users, user),
+    getByEmail: (email) => axiosService.get(`${urls.users}/${ email }`)
+}
